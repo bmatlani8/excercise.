13223ea1f3d8bc56b8967b72daeecbf8746459d9
bmatlani8 <91146468+bmatlani8@users.noreply.github.com>
2021-09-23 22:12:29 +0530

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+x = 10
+y = 20
+if (x>y):     
+ print(x+y)
+else:
+ print (x-y)
+
+
+# In[7]:
+
+
+print("Inspirational stories \n for \t Children")
+
+
+# In[10]:
+
+
+s = 0
+for I in range(10,2,-2):
+ s+=I
+print("sum= ",s)
+
+
+# In[13]:
+
+
+n = 50
+i = 5
+s = 0
+while i<n:
+  s+= i
+  i+= 10
+print("i=",i)
+print("sum=",s)
+
+
+# In[16]:
+
+
+y = 2000
+if (y%4==0):
+  print("Leap Year")
+else:
+  print("Not leap year")
+
+
+# In[26]:
+
+
+for i in range(10,301,10):
+   print(i)
+
+
+# In[27]:
+
+
+for i in range(105,8,-7):
+    print(i)
+
+
+# In[1]:
+
+
+i = 5
+while i < 101:
+  print(i)
+  i += 5
+
+
+# In[2]:
+
+
+i=100
+while i>2:
+    print(i)
+    i-=2
+
+
+# In[3]:
+
+
+for a in range(100,10,-10):
+  print(a)
+#9 times
+
+
+# In[ ]:
+
+
+for b in (1,10):
+ print(b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
